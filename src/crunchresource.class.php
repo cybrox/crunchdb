89e259428e9caea93ecc890ad57b68eab42ccbaf
Sven Marc Gehring <marc@cybrox.eu>
2014-11-04 11:37:05 +0100

    Implemented sorting

--- src/crunchresource.class.php
+++ src/crunchresource.class.php
@@ -24,9 +24,44 @@
      */
     public function __construct($base, $query){
       $this->data = $base->raw();
+      $this->apply($query);
     }
 
-    
+
+    /**
+     * Apply the given query to the dataset
+     * @param string $query The given query
+     */
+    private function apply($query){
+      $query = trim($query);
+
+      if($query == '*') return;
+    }
+
+
+    /**
+     * Sort the resource data with simple, predefined parameters
+     * @param mixed $x function arguments
+     * @return instance $this For chaining
+     */
+    public function sort(){
+      $args = func_get_args();
+      $args = is_array($args[0]) ? $args[0] : $args;
+      usort($this->data, $this->_cmp($args));
+      return $this;
+    }
+
+
+    /**
+     * Sort the resource data with a custom function
+     * @param function $func The user defined function
+     * @return instance $this For chaining
+     */
+    public function sortfn($func){
+      $this->data = usort($this->data, $func);
+      return $this;
+    }
+
 
     /**
      * Fetch the data in the current resource
@@ -43,6 +78,40 @@
      */
     public function count(){
       return count($this->data);
+    }
+
+
+    /**
+     * Generate a multi dimensional array compare method for usort
+     * Written by Jon ( http://stackoverflow.com/a/16788610 )
+     */
+    private function _cmp() {
+      $criteria = func_get_args();
+      foreach ($criteria as $index => $criterion) {
+        $criteria[$index] = is_array($criterion)
+          ? array_pad($criterion, 3, null)
+          : array($criterion, SORT_ASC, null);
+      }
+
+      return function($first, $second) use (&$criteria) {
+        foreach ($criteria as $criterion) {
+          list($column, $sortOrder, $projection) = $criterion;
+          $sortOrder = $sortOrder === SORT_DESC ? -1 : 1;
+
+          if ($projection) {
+            $lhs = call_user_func($projection, $first[$column]);
+            $rhs = call_user_func($projection, $second[$column]);
+          } else {
+            $lhs = $first[$column];
+            $rhs = $second[$column];
+          }
+
+          if ($lhs < $rhs) { return -1 * $sortOrder; }
+          else if ($lhs > $rhs) { return 1 * $sortOrder; }
+        }
+
+        return 0;
+      };
     }
 
   }
